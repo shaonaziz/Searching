c1e32826a6fa28716725ee0cd34a2b327cc703d8
azizJ99 <abdulaziz.dcastalia@gmail.com>
2022-08-18 11:58:12 +0600

    data-searching

---
+++ linearSearch.js
@@ -0,0 +1,16 @@
+var beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie'];
+
+beasts.indexOf('Godzilla');
+
+beasts.findIndex(function(item){
+  return item === 'Godzilla';
+});
+
+beasts.find(function(item){
+  return item === 'Godzilla';
+})
+
+beasts.includes('Godzilla')
+
+//best case if we find the item at the begining which O(1)
+//worst case if we find the item at the end which O(n)
